d91c79fd03f0a28b4e41b4024a06fd02bd4c99a2
LLR <luis.lasierra@luisla.com>
2023-07-25 09:26:09 +0200

    Initial Commit

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Luis Lasierra on 25/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
